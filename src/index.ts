7775e63aa5dc42dcedb54161b5981c072a86d155
Hazork <arthur.fiorette@gmail.com>
2021-11-11 16:06:19 -0300

    feat: export AxiosStorage

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ export * from './cache/cache';
 export * from './cache/create';
 export * from './header/types';
 export * from './interceptors/types';
+export * from './storage/storage';
 export * from './storage/types';
 export * from './util/types';
+
