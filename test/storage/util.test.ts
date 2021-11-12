ce779770d29d7fdcebdc93790efe74788a0c2589
Hazork <arthur.fiorette@gmail.com>
2021-11-12 20:42:28 -0300

    test: added keepIfStale tests

--- test/storage/util.test.ts
+++ test/storage/util.test.ts
@@ -1,10 +1,66 @@
-import type { AxiosStorage } from '../../src/storage/storage';
+import { AxiosStorage } from '../../src/storage/storage';
+import { Header } from '../../src/util/headers';
 
-describe('tests common storages', () => {
-  it('', () => {
-    //TODO: Add storage tests
-    const a: AxiosStorage | false = false;
+describe('tests abstract storages', () => {
+  it('tests storage keep if stale method', () => {
+    const etag = AxiosStorage.keepIfStale({
+      state: 'cached',
+      // Reverse to be ~infinity
+      createdAt: 1,
+      ttl: Date.now(),
+      data: {
+        status: 200,
+        statusText: '200 OK',
+        data: true,
+        headers: {
+          [Header.ETag]: 'W/"123"'
+        }
+      }
+    });
+    expect(etag).toBe(true);
 
-    expect(a).toBe(false);
+    const modifiedSince = AxiosStorage.keepIfStale({
+      state: 'cached',
+      // Reverse to be ~infinity
+      createdAt: 1,
+      ttl: Date.now(),
+      data: {
+        status: 200,
+        statusText: '200 OK',
+        data: true,
+        headers: {
+          [Header.IfModifiedSince]: new Date().toUTCString()
+        }
+      }
+    });
+    expect(modifiedSince).toBe(true);
+
+    const empty = AxiosStorage.keepIfStale({
+      state: 'cached',
+      // Reverse to be ~infinity
+      createdAt: 1,
+      ttl: Date.now(),
+      data: {
+        status: 200,
+        statusText: '200 OK',
+        data: true,
+        headers: {}
+      }
+    });
+    expect(empty).toBe(false);
+
+    const rest = AxiosStorage.keepIfStale({
+      state: 'cached',
+      // Reverse to be ~infinity
+      createdAt: 1,
+      ttl: Date.now(),
+      data: {
+        status: 200,
+        statusText: '200 OK',
+        data: true,
+        headers: undefined as any
+      }
+    });
+    expect(rest).toBe(false);
   });
 });
