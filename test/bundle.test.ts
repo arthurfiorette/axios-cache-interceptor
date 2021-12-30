1e251ddef3ec64177797c583f72ef3cfc2bffb22
arthurfiorette <arthur.fiorette@gmail.com>
2021-12-30 16:03:00 -0300

    test: added index.development tests

--- test/bundle.test.ts
+++ test/bundle.test.ts
@@ -4,12 +4,28 @@ import { MemoryAxiosStorage } from '../src/storage/memory';
 import { AxiosStorage } from '../src/storage/storage';
 
 describe('test bundle imports', () => {
-  it('should have basic storages', async () => {
+  it('tests browser ', async () => {
     const bundle = await import('../src/index.browser');
 
     expect(bundle.setupCache).toBe(setupCache);
     expect(bundle.AxiosStorage).toBe(AxiosStorage);
     expect(bundle.BrowserAxiosStorage).toBe(BrowserAxiosStorage);
     expect(bundle.MemoryAxiosStorage).toBe(MemoryAxiosStorage);
+  });
+
+  it('should have basic storages', async () => {
+    const oldWarn = console.warn;
+    console.warn = jest.fn();
+
+    const bundle = await import('../src/index.development');
+
+    expect(console.warn).toHaveBeenCalledTimes(1);
+
+    expect(bundle.setupCache).toBe(setupCache);
+    expect(bundle.AxiosStorage).toBe(AxiosStorage);
+    expect(bundle.BrowserAxiosStorage).toBe(BrowserAxiosStorage);
+    expect(bundle.MemoryAxiosStorage).toBe(MemoryAxiosStorage);
+
+    console.warn = oldWarn;
   });
 });
