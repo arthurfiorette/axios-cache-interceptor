500ac715239c27dfaa62e5f969ca08a5d8b04371
arthurfiorette <arthur.fiorette@gmail.com>
2022-01-03 09:13:02 -0300

    refactor: added data property in memory storages

--- src/storage/memory.ts
+++ src/storage/memory.ts
@@ -1,10 +1,34 @@
 import { buildStorage } from './build';
 import type { StorageValue } from './types';
 
-export function buildMemoryStorage(obj: Record<string, StorageValue> = {}) {
-  return buildStorage({
-    find: (key) => Promise.resolve(obj[key]),
-    set: (key, value) => Promise.resolve(void (obj[key] = value)),
-    remove: (key) => Promise.resolve(void delete obj[key])
+/**
+ * Creates a simple in-memory storage. This means that if you need to persist data between
+ * page or server reloads, this will not help.
+ *
+ * This is the storage used by default.
+ *
+ * If you need to modify it's data, you can do by the `data` property.
+ *
+ * @example
+ *
+ * ```js
+ * const memoryStorage = buildMemoryStorage();
+ *
+ * setupCache(axios, { storage: memoryStorage });
+ *
+ * // Simple example to force delete the request cache
+ *
+ * const { id } = axios.get('url');
+ *
+ * delete memoryStorage.data[id];
+ * ```
+ */
+export function buildMemoryStorage() {
+  const data: Record<string, StorageValue> = {};
+  const storage = buildStorage({
+    find: (key) => Promise.resolve(data[key]),
+    set: (key, value) => Promise.resolve(void (data[key] = value)),
+    remove: (key) => Promise.resolve(void delete data[key])
   });
+  return { ...storage, data };
 }
