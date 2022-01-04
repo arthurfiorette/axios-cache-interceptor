8d5a90e2fdbff90eea6cd63d2ef0b8a3faa2870f
arthurfiorette <arthur.fiorette@gmail.com>
2022-01-04 15:16:40 -0300

    fix: include default storages in cjs bundle

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,8 @@ export * from './cache/create';
 export * from './header/types';
 export * from './interceptors/types';
 export * from './storage/build';
+export * from './storage/memory';
 export * from './storage/types';
+export * from './storage/web-api';
+export * from './util/headers';
 export * from './util/types';
