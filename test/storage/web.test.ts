c63db52d8fab37d7070289ff57e7ee3430bff62e
arthurfiorette <arthur.fiorette@gmail.com>
2022-02-21 12:53:34 -0300

    tests: covered all cases

--- test/storage/web.test.ts
+++ test/storage/web.test.ts
@@ -9,8 +9,17 @@ describe('tests web storages', () => {
   testStorage('local-storage', () => buildWebStorage(localStorage));
   testStorage('session-storage', () => buildWebStorage(sessionStorage));
 
-  testStorageQuota('local-storage', () => (localStorage.clear(), localStorage));
-  testStorageQuota('session-storage', () => (sessionStorage.clear(), sessionStorage));
+  testStorageQuota('local-storage', () => {
+    // Clear previous values
+    localStorage.clear();
+    return localStorage;
+  });
+
+  testStorageQuota('session-storage', () => {
+    // Clear previous values
+    sessionStorage.clear();
+    return sessionStorage;
+  });
 });
 
 export function testStorageQuota(name: string, Storage: () => Storage): void {
@@ -32,8 +41,7 @@ export function testStorageQuota(name: string, Storage: () => Storage): void {
   });
 
   it(`tests buildWebStorage(${name}) function`, () => {
-    const storage = Storage();
-    const webStorage = buildWebStorage(storage);
+    const webStorage = buildWebStorage(Storage());
 
     expect(typeof webStorage.get).toBe('function');
     expect(typeof webStorage.set).toBe('function');
@@ -41,8 +49,7 @@ export function testStorageQuota(name: string, Storage: () => Storage): void {
   });
 
   it(`tests ${name} with gigant values`, async () => {
-    const storage = Storage();
-    const axios = mockAxios({ storage: buildWebStorage(storage) });
+    const axios = mockAxios({ storage: buildWebStorage(Storage()) });
 
     // Does not throw error
     await axios.storage.set('key', {
@@ -57,11 +64,15 @@ export function testStorageQuota(name: string, Storage: () => Storage): void {
   });
 
   it(`tests ${name} evicts oldest first`, async () => {
-    const storage = Storage();
-    const axios = mockAxios({ storage: buildWebStorage(storage) });
+    const axios = mockAxios({ storage: buildWebStorage(Storage()) });
 
     // Fills the storage with 5 keys
     for (const i of [1, 2, 3, 4, 5]) {
+      await axios.storage.set(`dummy-${i}`, {
+        state: 'loading',
+        previous: 'empty'
+      });
+
       await axios.storage.set(`key-${i}`, {
         state: 'cached',
         createdAt: Date.now(),
@@ -90,7 +101,54 @@ export function testStorageQuota(name: string, Storage: () => Storage): void {
 
     // Has evicted all 1-5 keys
     for (const i of [1, 2, 3, 4]) {
-      expect(await axios.storage.get(`key-${i}`)).toStrictEqual({ state: 'empty' });
+      const { state } = await axios.storage.get(`key-${i}`);
+      expect(state).toBe('empty');
+    }
+  });
+
+  it(`expects ${name} remove expired keys`, async () => {
+    const axios = mockAxios({ storage: buildWebStorage(Storage()) });
+
+    const year2k = new Date(2000, 1, 1);
+
+    // Fills the storage with 5 keys
+    // Each 10K ms newer than the previous one
+    for (const i of [1, 2, 3, 4, 5]) {
+      await axios.storage.set(`dummy-${i}`, {
+        state: 'loading',
+        previous: 'empty'
+      });
+
+      await axios.storage.set(`expired-${i}`, {
+        state: 'cached',
+        createdAt: year2k.getTime(),
+        ttl: i * 10_000,
+        data: {
+          ...EMPTY_RESPONSE,
+          data: '0'.repeat(MAXIMUM_LIMIT * 0.2) // 20% each
+        }
+      });
+    }
+
+    await axios.storage.set('non-expired', {
+      state: 'cached',
+      createdAt: Date.now(), // today
+      ttl: 10_000,
+      data: {
+        ...EMPTY_RESPONSE,
+        data: '0'.repeat(MAXIMUM_LIMIT * 0.9) // 90%
+      }
+    });
+
+    const initial = await axios.storage.get('non-expired');
+
+    // Key was defined
+    expect(initial.state).toBe('cached');
+
+    // Has evicted all 1-5 keys
+    for (const i of [1, 2, 3, 4]) {
+      const { state } = await axios.storage.get(`expired-${i}`);
+      expect(state).toBe('empty');
     }
   });
 }
