1b01590556e58da1cec9c0fc842c34db663bcbb3
arthurfiorette <arthur.fiorette@gmail.com>
2022-02-21 12:27:13 -0300

    feat: catch web storage quota errors

--- src/storage/web-api.ts
+++ src/storage/web-api.ts
@@ -1,5 +1,5 @@
-import type { StorageValue } from '..';
-import { buildStorage } from './build';
+import { buildStorage, canStale, isExpired } from './build';
+import type { StorageValue } from './types';
 
 /**
  * Creates a simple storage. You can persist his data by using `sessionStorage` or
@@ -22,26 +22,60 @@ import { buildStorage } from './build';
  *   multiple places using the same storage.
  */
 export function buildWebStorage(storage: Storage, prefix = '') {
-  const isStorageExceededError = (error: unknown): error is DOMException => {
-    const exceptionName = (error as DOMException).name;
-    return exceptionName === 'QuotaExceededError' && storage.length > 0;
-  };
   return buildStorage({
     find: (key) => {
       const json = storage.getItem(prefix + key);
       return json ? (JSON.parse(json) as StorageValue) : undefined;
     },
-    set: (key, value) => {
-      try {
-        storage.setItem(prefix + key, JSON.stringify(value));
-      } catch (error) {
-        if (isStorageExceededError(error)) {
-          storage.clear();
-        }
-      }
-    },
+
     remove: (key) => {
       storage.removeItem(prefix + key);
+    },
+
+    set: (key, value) => {
+      const save = () => storage.setItem(prefix + key, JSON.stringify(value));
+
+      try {
+        return save();
+      } catch (error) {
+        const allValues: [string, StorageValue][] = Object.entries(
+          storage as Record<string, string>
+        )
+          .filter(([key]) => key.startsWith(prefix) && storage.getItem(key))
+          .map(([key, val]) => [key, JSON.parse(val) as StorageValue]);
+
+        // Remove all expired values
+        for (const [prefixedKey, value] of allValues) {
+          if (value.state === 'cached' && isExpired(value) && !canStale(value)) {
+            storage.removeItem(prefixedKey);
+          }
+        }
+
+        // Try save again after removing expired values
+        try {
+          return save();
+        } catch (_) {
+          // Storage still full, try removing the oldest value until it can be saved
+
+          // Descending sort by createdAt
+          const sortedItems = allValues.sort(
+            ([, valueA], [, valueB]) => (valueA.createdAt || 0) - (valueB.createdAt || 0)
+          );
+
+          for (const [prefixedKey] of sortedItems) {
+            storage.removeItem(prefixedKey);
+
+            try {
+              return save();
+            } catch (_) {
+              // This key didn't free all the required space
+            }
+          }
+        }
+
+        // Clear the cache for the specified key
+        storage.removeItem(prefix + key);
+      }
     }
   });
 }
