4e9372810a0da7a9ca9ce371197fb25a1ba993e6
arthurfiorette <arthur.fiorette@gmail.com>
2022-03-14 17:55:20 -0300

    chore: fixed escheck script

--- build/check.sh
+++ build/check.sh
@@ -3,7 +3,7 @@
 # This script is used to check the umd's ecmascript compatibility.
 # It is intended to be run from the project's root directory.
 
-yarn esist/index.cjs dev/index.cjs dev/index.bundle.js-check es2017 d
+yarn es-check es2017 dist/index.cjs dev/index.cjs dev/index.bundle.js
 
 if [ $? -eq 1 ]; then
   exit 1
