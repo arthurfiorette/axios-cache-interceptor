90163aba1537955e2efdf8c5578f6100a6140efd
arthurfiorette <arthur.fiorette@gmail.com>
2022-12-11 18:58:58 -0300

    test: add test

--- test/storage/web.test.ts
+++ test/storage/web.test.ts
@@ -1,6 +1,7 @@
 /** @jest-environment jsdom */
 
 import { buildWebStorage } from '../../src/storage/web-api';
+import { mockAxios } from '../mocks/axios';
 import { testStorageQuota } from './quota';
 import { testStorage } from './storages';
 
@@ -14,5 +15,22 @@ describe('tests web storages', () => {
   afterEach(() => {
     localStorage.clear();
     sessionStorage.clear();
+  });
+
+  it('Should use a key prefix by default', async () => {
+    const storage = sessionStorage; // does not matter any implementation details
+
+    storage.setItem('test-key', '1');
+
+    const axios = mockAxios({
+      storage: buildWebStorage(storage)
+    });
+
+    await axios.get('url', {
+      id: 'test-key'
+    });
+
+    expect(storage.getItem('test-key')).toBe('1');
+    expect(axios.storage.get('test-key')).not.toBe('1');
   });
 });
