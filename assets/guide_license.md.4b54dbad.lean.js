358784c1bc7e8300cbb4e5310f13d816f93de45d
arthurfiorette <arthurfiorette@users.noreply.github.com>
2022-12-22 19:29:52 +0000

    Deploy docs for latest

---
+++ assets/guide_license.md.4b54dbad.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,d as s}from"./app.9f751a4b.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"guide/license.md","lastUpdated":1670290591000}'),o={name:"guide/license.md"},c=s("",3),n=[c];function r(i,p,l,_,d,g){return a(),t("div",null,n)}const m=e(o,[["render",r]]);export{u as __pageData,m as default};
