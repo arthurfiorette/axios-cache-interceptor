6835df0967995bef94c5abf9761ec3559fe258d4
arthurfiorette <arthurfiorette@users.noreply.github.com>
2023-01-29 01:30:41 +0000

    Deploy docs for latest

---
+++ assets/guide_license.md.c58f25d1.js
@@ -0,0 +1,22 @@
+import{_ as s,c as n,o as a,d as e}from"./app.95790de0.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"guide/license.md","lastUpdated":1674955467000}'),p={name:"guide/license.md"},l=e(`<div class="language- line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-palenight"><code><span class="line"><span style="color:#A6ACCD;">MIT License</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">Copyright (c) 2021-present Arthur Fiorette &amp; Contributors</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">Permission is hereby granted, free of charge, to any person obtaining a</span></span>
+<span class="line"><span style="color:#A6ACCD;">copy of this software and associated documentation files (the &quot;Software&quot;),</span></span>
+<span class="line"><span style="color:#A6ACCD;">to deal in the Software without restriction, including without limitation</span></span>
+<span class="line"><span style="color:#A6ACCD;">the rights to use, copy, modify, merge, publish, distribute, sublicense,</span></span>
+<span class="line"><span style="color:#A6ACCD;">and/or sell copies of the Software, and to permit persons to whom the</span></span>
+<span class="line"><span style="color:#A6ACCD;">Software is furnished to do so, subject to the following conditions:</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">The above copyright notice and this permission notice shall be included in</span></span>
+<span class="line"><span style="color:#A6ACCD;">all copies or substantial portions of the Software.</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR</span></span>
+<span class="line"><span style="color:#A6ACCD;">IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,</span></span>
+<span class="line"><span style="color:#A6ACCD;">FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE</span></span>
+<span class="line"><span style="color:#A6ACCD;">AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER</span></span>
+<span class="line"><span style="color:#A6ACCD;">LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING</span></span>
+<span class="line"><span style="color:#A6ACCD;">FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER</span></span>
+<span class="line"><span style="color:#A6ACCD;">DEALINGS IN THE SOFTWARE.</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br></div></div><br><p><a href="https://app.fossa.com/projects/git%2Bgithub.com%2Farthurfiorette%2Faxios-cache-interceptor?ref=badge_large" alt="FOSSA Status"><img src="https://app.fossa.com/api/projects/git%2Bgithub.com%2Farthurfiorette%2Faxios-cache-interceptor.svg?type=large"></a></p>`,3),t=[l];function o(r,i,c,A,b,C){return a(),n("div",null,t)}const T=s(p,[["render",o]]);export{m as __pageData,T as default};
