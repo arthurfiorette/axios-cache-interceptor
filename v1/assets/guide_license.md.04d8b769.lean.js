6c3fe0322258cf7707edb30513ed5a9778be43e7
arthurfiorette <arthurfiorette@users.noreply.github.com>
2023-01-29 01:30:27 +0000

    Deploy docs for v1

---
+++ v1/assets/guide_license.md.04d8b769.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,d as e}from"./app.13a892cf.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"guide/license.md","lastUpdated":1674955467000}'),p={name:"guide/license.md"},l=e("",3),t=[l];function o(r,i,c,A,b,C){return a(),n("div",null,t)}const T=s(p,[["render",o]]);export{m as __pageData,T as default};
