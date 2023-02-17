7a6f865bc5ab9f248b7e610de5e0a5228d1287d1
arthurfiorette <arthur.fiorette@gmail.com>
2023-02-16 23:55:34 -0300

    ci: fix pnpm test

--- build/check.sh
+++ build/check.sh
@@ -3,19 +3,19 @@
 # This script is used to check the umd's ecmascript compatibility.
 # It is intended to be run from the project's root directory.
 
-yarn es-check es2017 dist/index.cjs dev/index.cjs
+pnpm es-check es2017 dist/index.cjs dev/index.cjs
 
 if [ $? -eq 1 ]; then
   exit 1
 fi
 
-yarn es-check es2017 dist/index.mjs dev/index.mjs --module
+pnpm es-check es2017 dist/index.mjs dev/index.mjs --module
 
 if [ $? -eq 1 ]; then
   exit 1
 fi
 
-yarn es-check es5 dist/index.bundle.js
+pnpm es-check es5 dist/index.bundle.js
 
 if [ $? -eq 1 ]; then
   exit 1
