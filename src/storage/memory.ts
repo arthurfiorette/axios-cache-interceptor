4837429b14eb6b98c800815dcbf127305d7e92eb
Benjamin Hallion <bhallion@ohbibi.com>
2023-06-28 16:24:48 +0200

    fix lint issue

--- src/storage/memory.ts
+++ src/storage/memory.ts
@@ -31,25 +31,22 @@ declare const structuredClone: (<T>(value: T) => T) | undefined;
  * ```
  *
  * @param {boolean} cloneData If the data returned by `find()` should be cloned to avoid
- *   mutating the original data outside the `set()` method.
- *   Disabled is default
+ *   mutating the original data outside the `set()` method. Disabled is default
  * @param {number | false} cleanupInterval The interval in milliseconds to run a
  *   setInterval job of cleaning old entries. If false, the job will not be created.
  *   Disabled is default
  * @param {number | false} maxEntries The maximum number of entries to keep in the
  *   storage. Its hard to determine the size of the entries, so a smart FIFO order is used
  *   to determine eviction. If false, no check will be done and you may grow up memory
- *   usage.
- *   Disabled is default
+ *   usage. Disabled is default
  * @param {boolean} storeClone If the data stored by `set()` should be cloned to prevent
- *   future mutation from affecting cached data.
- *   Disabled is default
+ *   future mutation from affecting cached data. Disabled is default
  */
 export function buildMemoryStorage(
   cloneData = false,
   cleanupInterval: number | false = false,
   maxEntries: number | false = false,
-  storeClone = false,
+  storeClone = false
 ) {
   const storage = buildStorage({
     set: (key, value) => {
