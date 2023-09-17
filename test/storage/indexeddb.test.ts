d9e0f223909e7fda18b3ea78ce36dbda0d9fccc8
Luca Jung <luca@luca-jung.de>
2023-09-17 22:52:11 +0200

    refactor test

--- test/storage/indexeddb.test.ts
+++ test/storage/indexeddb.test.ts
@@ -8,7 +8,7 @@ import "fake-indexeddb/auto";
 
 const INDEXED_DB_TEST_NAME = 'axios-cache-test-indexeddb';
 
-async function getTestIndexedDb() {
+function getTestIndexedDb() {
   let db: IDBDatabase | undefined = undefined;
   const request = indexedDB.open(INDEXED_DB_TEST_NAME, 1);
 
@@ -19,22 +19,15 @@ async function getTestIndexedDb() {
   request.onsuccess = () => {
     db = request.result;
   };
-  return await buildIndexedDbStorage(db as unknown as IDBDatabase);
-};
+  return buildIndexedDbStorage(db as unknown as IDBDatabase);
+}
 
-// Copied from the memory.test.ts file
-describe('IndexedDBStorage', async () => {
+describe('IndexedDBStorage', () => {
   afterEach(() => {
     indexedDB.deleteDatabase(INDEXED_DB_TEST_NAME);
   });
   
-  testStorage('IndexedDBStorage', await getTestIndexedDb());
-
-  // Expects that when a result returned by storage.get() has his inner properties updated,
-  // a new request to storage.get() should maintain the same value.
-  //
-  // https://github.com/arthurfiorette/axios-cache-interceptor/issues/163
-  it('not allow changes by value reference', async () => {
+  it('should not allow changes by value reference', async () => {
     const storage = await getTestIndexedDb();
 
     await storage.set('key', {
@@ -59,5 +52,11 @@ describe('IndexedDBStorage', async () => {
     assert.notEqual(result2, null);
     assert.equal(result2.state, 'cached');
     assert.equal(result2.data?.data, 'data');
+  });
+
+  // Fügen Sie hier den TestStorage-Test hinzu
+  it('should pass the testStorage test', async () => {
+    const storage = await getTestIndexedDb();
+    await testStorage('IndexedDBStorage', storage);
   });
 });
